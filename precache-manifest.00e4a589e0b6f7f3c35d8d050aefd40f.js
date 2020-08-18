acb7d67e3041b4aa7e00b70c6eac60631fe17672
Adrian Kolev <adrian.kolev@netea.bg>
2020-08-18 16:22:13 +0300

    Updates

---
+++ precache-manifest.00e4a589e0b6f7f3c35d8d050aefd40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab98933356b4fc3e05d09630cdd08a0",
+    "url": "/create-react-app-repo/index.html"
+  },
+  {
+    "revision": "b717696af5e7060b9fa3",
+    "url": "/create-react-app-repo/static/css/main.84d84195.chunk.css"
+  },
+  {
+    "revision": "1d1f9ff9705abc48732c",
+    "url": "/create-react-app-repo/static/js/2.7208ebd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/create-react-app-repo/static/js/2.7208ebd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b717696af5e7060b9fa3",
+    "url": "/create-react-app-repo/static/js/main.3e84eaf5.chunk.js"
+  },
+  {
+    "revision": "46374ff5bf43b554369c",
+    "url": "/create-react-app-repo/static/js/runtime-main.232505dd.js"
+  }
+]);
